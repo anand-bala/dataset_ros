39378fdec227df31e3a42e28b4a9266736766584
Anand Balakrishnan <anandbala1597@gmail.com>
2018-03-26 23:52:51 -0400

    Loop through trajectories and dump 2D pose to file.

--- src/dump_trajectory.cc
+++ src/dump_trajectory.cc
@@ -2,9 +2,10 @@
 
 #include "cartographer/io/proto_stream.h"
 #include "cartographer/mapping/proto/pose_graph.pb.h"
-#include "cartographer/mapping/proto/trajectory_builder_options.pb.h"
+#include "cartographer/mapping/proto/trajectory.pb.h"
+
 #include "google/protobuf/text_format.h"
-#include <google/protobuf/io/zero_copy_stream_impl.h>
+#include "google/protobuf/io/zero_copy_stream_impl.h"
 
 #include "gflags/gflags.h"
 #include "glog/logging.h"
@@ -15,7 +16,7 @@
 DEFINE_string(pbfile, "",
     "Path to the protobuf stream file outputted by cartographer");
 
-DEFINE_string(outfile, "trajectory.txt",
+DEFINE_string(outfile, "trajectory.csv",
     "Path to the file where you want to output the trajectory."
     "The directory where the file is present must exist");
 
@@ -24,11 +25,29 @@ namespace dataset_ros {
     cartographer::io::ProtoStreamReader reader(pbfile);
     cartographer::mapping::proto::PoseGraph posegraph;
     CHECK(reader.ReadProto(&posegraph)) << "unable to read pbfile";
+    LOG(INFO) << "Number of Trajectories: " << posegraph.trajectory_size();
 
-    std::ofstream output_file;
-    output_file.open(outfile);
-    google::protobuf::TextFormat::Print(posegraph,
-        new google::protobuf::io::OstreamOutputStream(&output_file));
+    std::ofstream ofile;
+    ofile.open(outfile, std::ios::out | std::ios::trunc );
+    ofile << "TrajectoryID,NodeID,Timestamp,PositionX,PositionY,QuaternionZ,QuaternionW," << std::endl;
+
+    for (int i = 0; i < posegraph.trajectory_size(); i++) {
+      LOG(INFO) << "Trajectory " << i + 1;
+      const cartographer::mapping::proto::Trajectory &t = posegraph.trajectory(i);
+      for(int j = 0; j < t.node_size(); j++) {
+        LOG(INFO) << "-- Node " << j + 1;
+        const cartographer::mapping::proto::Trajectory::Node &n = t.node(j);
+        ofile << (i+1) << ',' << j+1 << ','
+          << n.timestamp() << ','
+          << n.pose().translation().x() << ','
+          << n.pose().translation().y() << ','
+          << n.pose().rotation().z() << ','
+          << n.pose().rotation().w() << ','
+          << std::endl;
+      }
+    }
+
+    ofile.close();
   }
 } // namespace dataset_ros
 
