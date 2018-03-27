3cc8a5d15cb399cf6d3c9cba5399095da94c9c93
Anand Balakrishnan <anandbala1597@gmail.com>
2018-03-26 20:49:01 -0400

    dump_trajectory now dumps to a file
    NOTES:
      - The initial bit is constraints. We should use only the trajectory
        field.
      - The output file seems to still be incomplete.
    TODO:
      - Iterate over trajectory field in pb and output the intividual
        trajectory entries into a file.

--- src/dump_trajectory.cc
+++ src/dump_trajectory.cc
@@ -4,22 +4,31 @@
 #include "cartographer/mapping/proto/pose_graph.pb.h"
 #include "cartographer/mapping/proto/trajectory_builder_options.pb.h"
 #include "google/protobuf/text_format.h"
+#include <google/protobuf/io/zero_copy_stream_impl.h>
 
 #include "gflags/gflags.h"
 #include "glog/logging.h"
 
+#include <iostream>
+#include <fstream>
+
 DEFINE_string(pbfile, "",
     "Path to the protobuf stream file outputted by cartographer");
 
+DEFINE_string(outfile, "trajectory.txt",
+    "Path to the file where you want to output the trajectory."
+    "The directory where the file is present must exist");
 
 namespace dataset_ros {
-  void RunPbDump(std::string pbfile) {
+  void RunPbDump(std::string pbfile, std::string outfile) {
     cartographer::io::ProtoStreamReader reader(pbfile);
     cartographer::mapping::proto::PoseGraph posegraph;
     CHECK(reader.ReadProto(&posegraph)) << "unable to read pbfile";
-    std::string proto_str;
-    google::protobuf::TextFormat::PrintToString(posegraph, &proto_str);
-    LOG(INFO) << proto_str;
+
+    std::ofstream output_file;
+    output_file.open(outfile);
+    google::protobuf::TextFormat::Print(posegraph,
+        new google::protobuf::io::OstreamOutputStream(&output_file));
   }
 } // namespace dataset_ros
 
@@ -30,6 +39,8 @@ int main(int argc, char** argv) {
   google::ParseCommandLineFlags(&argc, &argv, true);
 
   CHECK(!FLAGS_pbfile.empty()) << "-pbfile missing.";
-  dataset_ros::RunPbDump(FLAGS_pbfile);
+  CHECK(!FLAGS_outfile.empty()) << "-outfile missing." 
+    << "Outputting to trajectory.txt";
+  dataset_ros::RunPbDump(FLAGS_pbfile, FLAGS_outfile);
 }
 
